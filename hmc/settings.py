952f56ac75e0570f27625928caf51d97c162e347
omar Lara <omarlara1987@gmail.com>
2024-02-15 16:21:29 -0500

    Made additional changes on allowed hosts to settings.py

--- hmc/settings.py
+++ hmc/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get("DEBUG")) == "1"
 
-ALLOWED_HOSTS = [os.environ.get("COMPANY_WEBSITE")]
+ALLOWED_HOSTS = [os.environ.get("COMPANY_WEBSITE"), os.environ.get("RAILWAY_DOMAIN")]
 
 if not DEBUG:
     ALLOWED_HOSTS += [os.environ.get("ALLOWED_HOST")]
