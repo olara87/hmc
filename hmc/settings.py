ac633722ecb364c25e945087e0e0573432d652fb
omar Lara <omarlara1987@gmail.com>
2024-02-16 02:50:29 -0500

    setup dj_database_url and remade procfile file.

--- hmc/settings.py
+++ hmc/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import os
-
+from pathlib import Path
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -109,6 +109,8 @@ DATABASES = {
 
     }
 }
+
+DATABASES["default"] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
